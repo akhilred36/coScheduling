1f5012d2fe8684b007d492c8538e374edd6ef64b
akhilred36 <akhilred36@gmail.com>
2023-10-10 13:38:35 -0600

    shared_ptr doesn't work with MPI

--- networkInhibitor/networkInhib.cpp
+++ networkInhibitor/networkInhib.cpp
@@ -1,8 +1,29 @@
-#include <iostream>
-#include <unistd.h>
 #include <mpi.h>
+#include <unistd.h>
 
-int main(int argc, char** argv) {
+#include <iostream>
+#include <memory>
+#include <random>
+#include <vector>
+
+#define ITERS 1000
+
+using namespace std;
+
+void fillArray(shared_ptr<double[]> array, int size)
+{
+    double lower_bound = 0;
+    double upper_bound = 1000000;
+    uniform_real_distribution<double> unif(lower_bound, upper_bound);
+    default_random_engine re;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = unif(re);
+    }
+}
+
+int main(int argc, char** argv)
+{
     MPI_Init(&argc, &argv);
     int rank, numProcs;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -13,8 +34,10 @@ int main(int argc, char** argv) {
     msgSize = 1000;
 
     int flags, opt;
-    while ((opt = getopt(argc, argv, "m:w:")) != -1) {
-        switch (opt) {
+    while ((opt = getopt(argc, argv, "m:w:")) != -1)
+    {
+        switch (opt)
+        {
             case 'm':
                 msgSize = atol(optarg);
                 break;
@@ -22,16 +45,51 @@ int main(int argc, char** argv) {
                 waitTime = atol(optarg);
                 break;
             default:
-                std::cerr << "Usage: " << argv[0] << " -m <message size (kb)> -w <wait time (ms)>" << std::endl;
+                cerr << "Usage: " << argv[0]
+                     << " -m <message size (kb)> -w <wait time (ms)>" << endl;
                 return -1;
         }
     }
     if (rank == 0)
     {
-        std::cout << "------------------------------------------------------------" << std::endl;
-        std::cout << "Using: \n\tMessage Size (kb): " << msgSize << ". Wait time (ms): " << waitTime <<
-                     "\n\tNumber of Processes: " << numProcs << std::endl;
-        std::cout << "------------------------------------------------------------" << std::endl;
+        cout << "------------------------------------------------------------"
+             << endl;
+        cout << "Using: \n\tMessage Size (kb): " << msgSize
+             << ". Wait time (ms): " << waitTime
+             << "\n\tNumber of Processes: " << numProcs << endl;
+        cout << "------------------------------------------------------------"
+             << endl;
     }
+    vector<shared_ptr<double[]>> sendBuffers;
+    vector<shared_ptr<double[]>> recvBuffers;
+
+    for (int i = 0; i < numProcs; i++)
+    {
+        shared_ptr<double[]> sb(new double[msgSize * 1000]);
+        shared_ptr<double[]> rb(new double[msgSize * 1000]);
+        sendBuffers.push_back(sb);
+        recvBuffers.push_back(rb);
+        fillArray(sendBuffers.at(i), msgSize * 1000);
+    }
+
+    shared_ptr<MPI_Request[]> sendReqs(new MPI_Request[numProcs]);
+    shared_ptr<MPI_Request[]> recvReqs(new MPI_Request[numProcs]);
+
+    for (int i = 0; i < ITERS; i++)
+    {
+        for (int j = 0; j < numProcs; j++)
+        {
+            MPI_Isend(&(sendBuffers.at(j)), msgSize * 1000, MPI_DOUBLE, j, 0,
+                      MPI_COMM_WORLD, &(sendReqs[j]));
+            MPI_Irecv(&(recvBuffers.at(j)), msgSize * 1000, MPI_DOUBLE, j, 0,
+                      MPI_COMM_WORLD, &(recvReqs[j]));
+        }
+        MPI_Waitall(numProcs, &(sendReqs[0]), MPI_STATUSES_IGNORE);
+        MPI_Waitall(numProcs, &(recvReqs[0]), MPI_STATUSES_IGNORE);
+        // Background network soaking job
+        // Run app
+        // Kill network soaking job when app finishes
+    }
+    MPI_Finalize();
     return 0;
 }
