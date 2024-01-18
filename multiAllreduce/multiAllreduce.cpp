2943074f59c8c948def7d6e02acc0806941d4f5e
akhilred36 <akhilred36@gmail.com>
2024-01-18 08:10:33 -0700

    Add multiAllreduce

--- multiAllreduce/multiAllreduce.cpp
+++ multiAllreduce/multiAllreduce.cpp
@@ -6,13 +6,9 @@
 
 void fillArray(double* array, int size)
 {
-    double lower_bound = 0;
-    double upper_bound = 1000000;
-    std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
-    std::default_random_engine re;
     for (int i = 0; i < size; i++)
     {
-        array[i] = unif(re);
+        array[i] = i + 1;
     }
 }
 
@@ -36,6 +32,7 @@ int main(int argc, char** argv)
                 break;
             case 'i':
                 iterations = atol(optarg);
+                break;
             default:
                 std::cerr << "Usage: " << argv[0]
                      << " -n <num elements per process> -i <num iterations>" << std::endl;
@@ -43,18 +40,25 @@ int main(int argc, char** argv)
         }
     }
 
-    double globalSum;
+    double globalSum = 0;
+    double localSum = 0;
     double* localElements = (double*) malloc(sizeof(double)*lengthPerProcess);
 
     fillArray(localElements, lengthPerProcess);
 
     for (int i = 0; i < iterations; i++)
     {
-        MPI_Allreduce(localElements, &globalSum, lengthPerProcess, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        globalSum = 0;
+        localSum = 0;
+        for (int j = 0; j < lengthPerProcess; j++)
+        {
+            localSum += localElements[j];
+        }
+        MPI_Allreduce(&localSum, &globalSum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     }
 
-    std::cout << "Global sum is: " << globalSum << std::endl;
-
+    std::cout << "Rank " << rank << " local sum: " << localSum << std::endl;
+    std::cout << "Rank " << rank << " global sum: " << globalSum << std::endl;
     free(localElements);
     MPI_Finalize();
     return 0;
