4854dddf65dcd80d2a6ac1bc04dda11f15ffb857
Akhil Reddy <akhilred36@gmail.com>
2026-06-08 10:35:50 -0600

    Revert "tolerance bug fix"
    This reverts commit 15f50f0693723f78b7e74625ffe7c4052482fe8f.

--- apps/AMG2023/amg.c
+++ apps/AMG2023/amg.c
@@ -571,7 +571,7 @@ main( hypre_int argc,
       HYPRE_PCGSetPrintLevel(pcg_solver, ioutdat);
       if (fixed_iterations > 0)
       {
-         HYPRE_PCGSetAbsoluteTol(pcg_solver, 0.0);
+         HYPRE_PCGSetAbsoluteTol(pcg_solver, 1e30);
       }
       else
       {
@@ -597,14 +597,7 @@ main( hypre_int argc,
       HYPRE_BoomerAMGSetKeepTranspose(pcg_precond, keepTranspose);
       HYPRE_BoomerAMGSetCumNnzAP(pcg_precond, cum_nnz_AP);
       HYPRE_BoomerAMGSetMaxRowSum(pcg_precond, 1.0);
-      if (fixed_iterations > 0)
-      {
-         HYPRE_PCGSetMaxIter(pcg_solver, fixed_iterations);
-      }
-      else
-      {
-         HYPRE_PCGSetMaxIter(pcg_solver, mg_max_iter);
-      }
+      HYPRE_PCGSetMaxIter(pcg_solver, mg_max_iter);
       HYPRE_PCGSetPrecond(pcg_solver,
                           (HYPRE_PtrToSolverFcn) HYPRE_BoomerAMGSolve,
                           (HYPRE_PtrToSolverFcn) HYPRE_BoomerAMGSetup,
@@ -732,7 +725,7 @@ main( hypre_int argc,
       }
      if (fixed_iterations > 0)
       {
-         HYPRE_GMRESSetAbsoluteTol(pcg_solver, 0.0);
+         HYPRE_GMRESSetAbsoluteTol(pcg_solver, 1e30);
       }
       else
       {
@@ -758,14 +751,7 @@ main( hypre_int argc,
       HYPRE_BoomerAMGSetKeepTranspose(pcg_precond, keepTranspose);
       HYPRE_BoomerAMGSetCumNnzAP(pcg_precond, cum_nnz_AP);
       HYPRE_BoomerAMGSetMaxRowSum(pcg_precond, max_row_sum);
-      if (fixed_iterations > 0)
-      {
-         HYPRE_GMRESSetMaxIter(pcg_solver, fixed_iterations);
-      }
-      else
-      {
-         HYPRE_GMRESSetMaxIter(pcg_solver, mg_max_iter);
-      }
+      HYPRE_GMRESSetMaxIter(pcg_solver, mg_max_iter);
       HYPRE_GMRESSetPrecond(pcg_solver,
                             (HYPRE_PtrToSolverFcn) HYPRE_BoomerAMGSolve,
                             (HYPRE_PtrToSolverFcn) HYPRE_BoomerAMGSetup,
