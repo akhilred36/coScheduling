d902a9d91c8cd021d4bf766ffd051c102f2973f7
Akhil Reddy <akhilred36@gmail.com>
2026-07-03 10:41:06 -0600

    Add temporary failed files analyzer

---
+++ temp/analyze_failed_files.py
@@ -0,0 +1,14 @@
+contents = open("failed_files.txt", 'r').read().split("\n")
+
+config_dict = {}
+
+for c in contents:
+    c_config = c[:-2]
+    if (c_config in config_dict.keys()):
+        config_dict[c_config] += 1
+    else:
+        config_dict[c_config] = 1
+
+for c in config_dict.keys():
+    if (config_dict[c] > 1):
+        print(c)
